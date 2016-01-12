78f68e618387600a4c01e1b99eeb6183e24bc544
a00926003 <a00926003@SE12L0320-10.ad.bcit.ca>
2016-01-12 14:07:41 -0800

    try it #3

--- index.php
+++ index.php
@@ -12,6 +12,14 @@
         $level = 10;
         echo 'Hi, my name is '.$name,'. and I am a level '.$level.'
         '.$what;
+        
+        echo '<br/>';
+        
+        $hoursworked = 10;
+        $rate = 12;
+        $total = $hoursworked * $rate;
+        echo 'You owe me '.$total;
+        
         ?>
     </body>
 </html>
