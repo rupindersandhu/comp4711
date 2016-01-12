598dbef64326aed98404ee303896ca7d03391df9
a00926003 <a00926003@SE12L0320-10.ad.bcit.ca>
2016-01-12 13:53:18 -0800

    index.php created

---
+++ index.php
@@ -0,0 +1,13 @@
+<DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/hrml; charset=UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        //
+        echo "Hi there";
+        ?>
+    </body>
+</html>
