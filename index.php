28fe6a85c7479da3898bcca975284ade2a2ff89b
a00926003 <a00926003@SE12L0320-10.ad.bcit.ca>
2016-01-12 14:17:07 -0800

    try it #4

--- index.php
+++ index.php
@@ -18,8 +18,14 @@
         $hoursworked = 10;
         $rate = 12;
         $total = $hoursworked * $rate;
-        echo 'You owe me '.$total;
         
+        if ($hoursworked > 40) {
+        $total = $hoursworked * $rate * 1.5;
+        } else {
+        $total = $hoursworked * $rate;
+        }
+        echo ($total > 0) ? 'You owe me '.$total : "You're welcome";
+
         ?>
     </body>
 </html>
