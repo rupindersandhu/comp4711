b338b2c6b8652b6d9b2d43222ad28709c624052f
a00926003 <a00926003@SE12L0320-10.ad.bcit.ca>
2016-01-12 13:59:59 -0800

    try it #1

--- index.php
+++ index.php
@@ -7,7 +7,15 @@
     <body>
         <?php
         //
-        echo "Hi there";
+        $temp = 'Jim';
+        echo 'Hi, my name is';
+        echo $temp;
+        $temp = 'geek';
+        echo "I am a";
+        echo $temp;
+        $temp = 10;
+        echo 'My level is';
+        echo $temp;
         ?>
     </body>
 </html>
