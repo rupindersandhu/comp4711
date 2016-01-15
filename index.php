59372e961d1521edfbc53b371a9f25d7566361bb
Kratos Sandhu <PirateSandhu@rupinders-MacBook-Pro.local>
2016-01-14 23:58:15 -0800

    try it #7
    replaces if-else with loops

--- index.php
+++ index.php
@@ -54,44 +54,31 @@
     function winner($token,$position) 
     {
         $won = false;
+        
+        
+        for($row=0; $row<3; $row++)
+        {
+            if (($position[3*$row] == $token) && 
+                ($position[3*$row+1] == $token) && 
+                ($position[3*$row+2] == $token))
+            {
+                $won = true;
+            } else if (($position[$row] == $token) && 
+                ($position[$row+3] == $token) && 
+                ($position[$row+6] == $token))
+            {
+                $won = true;
+            }   
+        }
+        
         if (($position[0] == $token) &&
-            ($position[1] == $token) &&
-            ($position[2] == $token)) 
-        {
-            $won = true;
-        } else if (($position[3] == $token) &&
-                   ($position[4] == $token) &&
-                    ($position[5] == $token)) 
-        {
-            $won = true;
-        } else if (($position[6] == $token) &&
-                   ($position[7] == $token) &&
-                    ($position[8] == $token)) 
-        {
-            $won = true;
-        } else if (($position[0] == $token) &&
-                   ($position[3] == $token) &&
-                    ($position[6] == $token)) 
-        {
-            $won = true;
-        } else if (($position[1] == $token) &&
-                   ($position[4] == $token) &&
-                    ($position[7] == $token)) 
+            ($position[5] == $token) &&
+            ($position[8] == $token)) 
         {
             $won = true;
         } else if (($position[2] == $token) &&
                    ($position[5] == $token) &&
-                    ($position[8] == $token)) 
-        {
-            $won = true;
-        } else if (($position[0] == $token) &&
-                   ($position[5] == $token) &&
-                    ($position[8] == $token)) 
-        {
-            $won = true;
-        } else if (($position[2] == $token) &&
-                   ($position[5] == $token) &&
-                    ($position[6] == $token)) 
+                   ($position[6] == $token)) 
         {
             $won = true;
         }
